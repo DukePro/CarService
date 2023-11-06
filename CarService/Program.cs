97c11cfc27f76ece662df18fc73f8fa1ef451dd7
D Egor <Mail.egor.d@gmail.com>
2023-11-06 22:20:56 +0300

    +

--- CarService/Program.cs
+++ CarService/Program.cs
@@ -1,6 +1,4 @@
-﻿using System.Linq;
-
-namespace CarService
+﻿namespace CarService
 {
     class Programm
     {
@@ -8,8 +6,8 @@ namespace CarService
         {
             //Menu menu = new Menu();
             //menu.Run();
-            Car car = new Car();
-            car.CreateCar();
+            CarFactory factory = new CarFactory();
+            Car car = new Car(factory.CreateCar());
             car.ShowCar();
         }
     }
@@ -28,8 +26,6 @@ namespace CarService
             bool isExit = false;
             int menuPositionY = 0;
 
-
-
             while (isExit == false)
             {
                 Console.SetCursorPosition(0, menuPositionY);
@@ -45,7 +41,6 @@ namespace CarService
                 {
                     case FishAdd:
                         UiOperations.CleanString();
-
                         break;
 
                     case FishRemove:
@@ -68,24 +63,53 @@ namespace CarService
         }
     }
 
-    class Service //содержит склад и счёт сервиса
+    class Service //содержит склад и счёт сервиса и ремонтирует машину клиента.
     {
         private int _serviceMoney;
 
     }
 
-    class Storage // содержит детали на замену
+    class Storage // содержит контейнеры с деталями
     {
+        private List<Container> _containers = new List<Container>();
 
+        public Storage(List<Container> containers)
+        {
+            _containers = containers;
+        }
+
+        private void AddPartsToExistingContainers(List<Container> containers) //Добавляет детали в контейнеры
+        {
+            for (int i = 0; i < containers.Count; i++)
+            {
+                for (int j = 0; j < _containers.Count; j++)
+                {
+                    if (containers[i].Name == _containers[j].Name)
+                    {
+                        _containers[i].AddParts(containers[j].TransferAllParts());
+                    }
+                }
+            }
+        }
     }
 
     class Container
     {
-        public Container(string name)
+        private static int _id = 0;
+        private List<Part> _parts;
+
+        public Container(List<Part> parts)
         {
-            Name = name;
-            Index = 0;
-            _parts = null;
+            Id = _id++;
+            
+            if (_parts == null)
+            {
+                _parts = parts;
+            }
+            else
+            {
+                _parts.AddRange(parts);
+            }
 
             if (_parts != null)
             {
@@ -95,24 +119,93 @@ namespace CarService
             {
                 Ammount = 0;
             }
+
+            if (_parts != null)
+            {
+                Name = _parts[0].Name;
+            }
+            else
+            {
+                Name = "заготовка";
+            }
         }
 
-        public int Index { get; private set; }
+        public int Id { get; private set; }
         public string Name { get; private set; }
         private int Ammount { get; set; }
-        private List<Part> _parts { get; set; }
+
+        public Part TransferPart()
+        {
+            if (_parts.Count > 0)
+            {
+                Part part = _parts[0];
+                _parts.RemoveAt(0);
+
+                return part;
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        public List<Part> TransferAllParts()
+        {
+            List<Part> parts = _parts;
+            _parts.Clear();
+
+            return parts;
+        }
+
+        public void AddParts(List<Part> parts)
+        {
+            _parts.AddRange(parts);
+        }
     }
 
     class Client //содержит деньги и машину
     {
-        private int _clientMoney;
+        private static int _id = 0;
 
+        public Client(Car car)
+        {
+            Id = _id++;
+            Money = 1000;
+            ClientCar = car;
+        }
+
+        public int Id { get; private set; }
+        public int Money { get; private set; }
+        public Car ClientCar { get; private set; }
+
+        public void ReciveRepearedCar(Car car)
+        {
+            ClientCar = car;
+        }
+
+        public int GiveMoney(int money)
+        {
+            if (Money - money < 0)
+            {
+                Console.WriteLine("Не хватает денег!");
+            }
+
+            Money =- money;
+            Console.WriteLine($"Клиент заплатил {money}");
+
+            return money;
+        }
+
+        public void ReciveMoney(int money)
+        {
+            Money =+ money;
+            Console.WriteLine($"Клиент получил {money}");
+        }
     }
 
-    class Car //содержит детали
+    class PartProvider
     {
-        private List<Part> _car = new List<Part>();
-        private Part[] _partTypes = new Part[] // Надо придумать как дать доступ на чтение этого массива другим классам... (возможно метод с выдачей копии массива)
+        private Part[] _partTypes = new Part[]
     {
     new Part("wheel"),
     new Part("right headlight"),
@@ -136,109 +229,141 @@ namespace CarService
     new Part("speedometer")
     };
 
-        public void CreateCar() //собираем тачку
+        public Part[] ProvideAllPartsType()
+        {
+            Part[] parts = new Part[] { };
+            parts = _partTypes;
+
+            return parts;
+        }
+    }
+
+    class CarFactory
+    {
+        PartProvider parts = new PartProvider();
+
+        public List<Part> CreateCar() //собираем тачку
         {
             int ammountOfWheels = 4;
             int ammountOfSeats = 4;
-            //int ammountByDefault = 1;
+            List<Part> car = new List<Part>();
 
-            for (int i = 0; i < _partTypes.Length; i++)
+            for (int i = 0; i < parts.ProvideAllPartsType().Length; i++)
             {
-                if (_partTypes[i].Name == "wheel")
+                if (parts.ProvideAllPartsType()[i].Name == "wheel")
                 {
                     for (int j = 0; j < ammountOfWheels; j++)
                     {
-                        _car.Add(new Part(_partTypes[i])); //добавляем не из массива, чтобы были уникальными.
+                        car.Add(new Part(parts.ProvideAllPartsType()[i])); //добавляем не из массива, чтобы были уникальными (используется другой конструктор).
                     }
                 }
-                else if (_partTypes[i].Name == "seat")
+                else if (parts.ProvideAllPartsType()[i].Name == "seat")
                 {
                     for (int j = 0; j < ammountOfSeats; j++)
                     {
-                        _car.Add(new Part(_partTypes[i]));
+                        car.Add(new Part(parts.ProvideAllPartsType()[i]));
                     }
                 }
                 else
                 {
-                    _car.Add(_partTypes[i] );
+                    car.Add(parts.ProvideAllPartsType()[i]);
                 }
             }
 
-            BrakeCar(); //ломаем тачку
+            car = BrakeCar(car); //ломаем тачку
+
+            return car;
+        }
+
+        private List<Part> BrakeCar(List<Part> car) // ломаем тачку
+        {
+            int brokenPartsProbability = 2; //на это число делим все детали в машине, чтобы получить макс допустимое кол-во поломок
+            int minAmmountOfBrokenParts = 1;
+            int maxAmmountOfBrokenParts = car.Count / brokenPartsProbability;
+            int ammountOfBrokenParts = Utils.GetRandomNumber(minAmmountOfBrokenParts, maxAmmountOfBrokenParts);
+            List<Part> tempCar = new List<Part>();
+
+            for (int i = 0; i < ammountOfBrokenParts; i++) //проходим по тачке столько раз, сколько выпало сломанных деталей, ломаем и перекидываем их во временную тачку
+            {
+                int brokenItemIndex = Utils.GetRandomNumber(car.Count - 1);
+                car[brokenItemIndex].BrakePart();
+                tempCar.Add(car[brokenItemIndex]);
+                car.RemoveAt(brokenItemIndex);
+            }
+
+            if (car.Count > 0) //докидываем целые детали во временную тачку
+            {
+                for (int i = 0; i < car.Count; i++)
+                {
+                    tempCar.Add(car[i]);
+                }
+
+                car = tempCar; //перекидываем из временной все детали в основную
+            }
+            else
+            {
+                car = tempCar;
+            }
+
+            //можно добавить сортировку по сломаным деталям и Id
+            return car;
+        }
+    }
+
+    class Car //содержит машну в виде листа деталей
+    {
+        private List<Part> _car;
+
+        public Car(List<Part> car)
+        {
+            _car = car;
         }
 
         public void ShowCar()
         {
-            int wheelCount = 0;//
-            int seatCount = 0;//
+            int wheelCount = 0;// временно
+            int seatCount = 0;// временно
 
             for (int i = 0; i < _car.Count; i++)
             {
                 _car[i].ShowPartInfo();
-                
-                if (_car[i].Name == "wheel")//
+
+                if (_car[i].Name == "wheel")// временно
                 {
-                    wheelCount++;//
+                    wheelCount++;// временно
                 }
-                if (_car[i].Name == "seat")//
+                if (_car[i].Name == "seat")// временно
                 {
-                    seatCount++;//
+                    seatCount++;// временно
                 }
             }
 
-            Console.WriteLine($"Колёс - {wheelCount}"); //
-            Console.WriteLine($"Сидений - {seatCount}");//
-        }
-
-        private void BrakeCar() // ломаем тачку
-        {
-            int brokenPartsProbability = 2; //на это число делим все детали в машине, чтобы получить макс допустимое кол-во поломок
-            int minAmmountOfBrokenParts = 1;
-            int maxAmmountOfBrokenParts = _car.Count / brokenPartsProbability;
-            int ammountOfBrokenParts = Utils.GetRandomNumber(minAmmountOfBrokenParts, maxAmmountOfBrokenParts);
-            List<Part> tempCar = new List<Part>();
-            
-            for (int i = 0; i < ammountOfBrokenParts; i++) //проходим по тачке столько раз, сколько выпало сломанных деталей, ломаем и перекидываем их во временную тачку
-            {
-                int brokenItemIndex = Utils.GetRandomNumber(_car.Count - 1);
-                _car[brokenItemIndex].IsBroken = true;
-                tempCar.Add(_car[brokenItemIndex]);
-                _car.RemoveAt(brokenItemIndex);
-            }
-
-            if (_car.Count > 0) //докидываем целые детали во временную тачку
-            {
-                for (int i = 0; i < _car.Count; i++)
-                {
-                    tempCar.Add(_car[i]);
-                }
-
-                _car = tempCar; //перекидываем из временной все детали в основную
-            }
-            else
-            {
-                _car = tempCar;
-            }
+            Console.WriteLine($"Колёс - {wheelCount}"); // временно
+            Console.WriteLine($"Сидений - {seatCount}");// временно
+            Console.WriteLine($"Всего деталей в авто - {_car.Count}");// временно
         }
     }
 
     class Part
     {
+        private static int _id = 0;
+
         public Part(string name)
         {
-            //Id = 0;
+            Id = _id++;
             Name = name;
             IsBroken = false;
         }
 
         public Part(Part name) //конструктор для копирования детали, а не клонирования.
         {
+            Id = _id++;
             Name = name.Name;
         }
 
-        //public int Id { get; private set; }
+        public int Id { get; private set; }
         public string Name { get; private set; }
-        public bool IsBroken { get; set; }
+        public bool IsBroken { get; private set; }
 
         public void ShowPartInfo()
         {
@@ -253,7 +378,12 @@ namespace CarService
                 status = "исправно";
             }
 
-            Console.WriteLine($"{Name} - {status}");
+            Console.WriteLine($"{Id} - {Name} - {status}");
+        }
+
+        public void BrakePart()
+        {
+            IsBroken = true;
         }
     }
 
@@ -312,6 +442,4 @@ namespace CarService
             Console.SetCursorPosition(0, Console.CursorTop);
         }
     }
-
-
 }
